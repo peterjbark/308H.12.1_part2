193390e7c04ebfdcf25be8a4ad39f2abf5af4a98
Peter <peterjbark@gmail.com>
2023-06-13 23:28:33 -0700

    Up to #11

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 var menuLinks = [
     {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
   ];
 
-let mainEl = document.querySelector("main");
+const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.className = "flex-ctr"
 mainEl.innerHTML = "<h1> DOM Manipulation </h1>" // Can't figure out how to utilize innerText to create Text using <h1> property
 
-let topMenuEl = document.querySelector("#top-menu");
+const topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%" ;
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.className = "flex-around"
@@ -21,3 +32,28 @@ for (links of menuLinks){
     aElement.innerText = links.text;
     topMenuEl.appendChild(aElement);
 }
+
+const subMenuEl = document.querySelector("#sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.className = "flex-around"
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+const showingSubMenu = false;
+const topMenuLinks = document.querySelectorAll("top-menu a");
+
+topMenuEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(event.target)
+if(event.target.tagName !== "A"){
+  console.log("not 'a' link");
+    return;
+}
+});
+
+
+// const test = document.querySelectorAll()
+// test.addEventListener("click",function(e){
+//   console.log(e)
+// })
